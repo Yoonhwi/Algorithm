706846ea2937721f5ebc041556256085c221e8d1
Yoonhwi <ush0105@naver.com>
2024-03-19 04:09:35 +0900

    practice algorithm

---
+++ programmers/LV2/H-Index.js
@@ -0,0 +1,21 @@
+const citations = [3, 0, 6, 1, 5];
+
+function solution(citations) {
+  let answer = 0;
+  answer = operate(citations, 0);
+  console.log(answer);
+  return answer;
+}
+
+function operate(citations, num) {
+  let count = 0;
+  citations.forEach((value) => {
+    value >= num ? count++ : count;
+  });
+  console.log("count:", count, "num:", num);
+  if (count >= num) {
+    return operate(citations, num + 1);
+  } else return num - 1;
+}
+
+solution(citations);
