996b8a0432b802ee7b9a17a0d06c452c25058b61
Yoonhwi <ush0105@naver.com>
2024-03-22 23:21:09 +0900

    practice algorithm

---
+++ 10000~19999/15000/15649.js
@@ -0,0 +1,57 @@
+{
+  const readline = require("readline");
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  let input;
+  let result = "";
+  const inputCb = (line) => {
+    input = line.split(" ").map(Number);
+  };
+
+  const closeCb = () => {
+    const [n, m] = input;
+    const seq = new Array(m).fill(0);
+    const visited = new Array(n).fill(false);
+    let result = "";
+    // function dfs(count) {
+    //   if (count === y) {
+    //     console.log(tempArray);
+    //     return;
+    //   }
+
+    //   for (i = 0; i < x; i++) {
+    //     if (dict[i]) continue;
+    //     dict[i] = true;
+    //     tempArray.push(i + 1);
+    //     dfs(count + 1);
+    //     tempArray.pop();
+    //     dict[i] = false;
+    //   }
+    // }
+    function dfs(k) {
+      if (k === m) {
+        const arr = [];
+        for (let i = 0; i < m; i++) {
+          arr.push(seq[i]);
+        }
+        return (result += `${arr.join(" ")}\n`);
+      }
+      for (let i = 1; i <= n; i++) {
+        if (!visited[i]) {
+          seq[k] = i;
+          visited[i] = true;
+          dfs(k + 1);
+          visited[i] = false;
+        }
+      }
+    }
+
+    dfs(0);
+    console.log(result);
+  };
+
+  rl.on("line", inputCb).on("close", closeCb);
+}
