395e00f00ad9878f56090351af1628a9b9dc5da1
Yoonhwi <ush0105@naver.com>
2024-05-14 14:26:13 +0900

    practice algorithm

--- programmers/LV2/프로세스.js
+++ programmers/LV2/프로세스.js
@@ -1,26 +1,22 @@
 function solution(priorities, location) {
-  let result = 0;
-  let target = location;
-  const arr = [...priorities].sort((a, b) => b - a);
-  let max = arr.shift();
+  let answer = 0;
 
-  for (i = 0; i < priorities.length; i++) {
-    if (priorities[i] >= max) {
-      result++;
-      if (i === target) return result;
-      max = arr.shift();
-      priorities.shift();
-      target = target > 0 ? target - 1 : arr.length;
-      i = 0;
+  const indexArr = priorities.map((v, i) => {
+    return i;
+  });
+
+  while (priorities.length) {
+    const max = Math.max(...priorities);
+    const curr = priorities[0];
+    if (curr < max) {
+      indexArr.push(indexArr.shift());
+      priorities.push(priorities.shift());
     } else {
-      target = target > 0 ? target - 1 : arr.length;
-      const first = priorities.shift();
-      priorities.push(first);
+      answer++;
+      if (indexArr.shift() === location) return answer;
+      priorities.shift();
     }
-    console.log(priorities, result, target);
   }
-
-  return result;
 }
 
-console.log(solution([2, 1, 3, 2], 2));
+console.log(solution([1, 1, 9, 1, 1, 1], 0));
