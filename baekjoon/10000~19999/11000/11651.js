67828744602e00fed482dee3418f074c6aabcda6
Yoonhwi <ush0105@naver.com>
2024-09-16 21:23:26 +0900

    baekjoon : 11651번 좌표 정렬하기2

---
+++ baekjoon/10000~19999/11000/11651.js
@@ -0,0 +1,26 @@
+//좌표 정렬하기2 (정렬)
+{
+  const readline = require("readline");
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  let input = [];
+
+  const inputCb = (line) => {
+    input.push(line.split(" ").map(Number));
+  };
+
+  const closeCb = () => {
+    input.shift();
+    input.sort((a, b) => {
+      if (a[1] < b[1]) return -1;
+      if (a[1] === b[1] && a[0] < b[0]) return -1;
+    });
+
+    console.log(input.map((v) => v.join(" ")).join("\n"));
+  };
+
+  rl.on("line", inputCb).on("close", closeCb);
+}
