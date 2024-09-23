1b2e81bad599afc8c5559365257dcb6aa89de5f2
Yoonhwi <ush0105@naver.com>
2024-09-23 21:18:41 +0900

    leetcode : retry [medium] depth-first-search

--- leetcode/medium/depth-first-search.js
+++ leetcode/medium/depth-first-search.js
@@ -14,32 +14,38 @@ var solve = function (board) {
     [0, -1],
   ];
 
-  for (let i = 0; i < m; i++) {
-    if (board[0][i] === "O" || board[n - 1][i] === "O") {
-      for (const [x, y] of dir) {
-        dfs(i + x, i + y);
-      }
-    }
-  }
-  for (let j = 0; j < n; j++) {
-    if (board[j][0] === "O" || board[j][m - 1] === "O") {
-      for (const [x, y] of dir) {
-        dfs(i + x, i + y);
-      }
-    }
-  }
   function dfs(i, j) {
-    if (i < 0 || j < 0 || i >= n || j >= m) return;
-    board[i][j] === "S";
+    if (i < 0 || j < 0 || i >= n || j >= m || board[i][j] !== "O") return;
+    board[i][j] = "S";
+    for (const [dx, dy] of dir) {
+      dfs(i + dx, j + dy);
+    }
   }
+
+  for (let i = 0; i < m; i++) {
+    if (board[0][i] === "O") dfs(0, i);
+    if (board[n - 1][i] === "O") dfs(n - 1, i);
+  }
+
+  for (let j = 0; j < n; j++) {
+    if (board[j][0] === "O") dfs(j, 0);
+    if (board[j][m - 1] === "O") dfs(j, m - 1);
+  }
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < m; j++) {
+      board[i][j] === "O" && (board[i][j] = "X");
+      board[i][j] === "S" && (board[i][j] = "O");
+    }
+  }
+
   return board;
 };
 
 const testCase = [
-  ["X", "X"],
-  ["X", "O"],
-  ["X", "X"],
-  ["X", "O"],
+  ["X", "O", "X"],
+  ["O", "X", "O"],
+  ["X", "O", "X"],
 ];
 
 console.log(solve(testCase));
