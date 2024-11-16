565c2a996ca502fede74c30b3c93c59c8eb3e4e1
Yoonhwi <ush0105@naver.com>
2024-11-16 18:09:49 +0900

    leetcode : [medium] 17.letters-combinations in JS

--- leetcode/medium/17.letter-combinations.js
+++ leetcode/medium/17.letter-combinations.js
@@ -20,7 +20,7 @@ var letterCombinations = function (digits) {
     return result;
   }
 
-  addString("", digits, result);
+  addString2("", digits, 0, result);
   return result;
 };
 
@@ -36,7 +36,21 @@ function addString(str, digits, result) {
   }
 }
 
-// 함수를 하나 만들고 , str를 받고 , digits문자를 받음, digit 문자가 없다면 result에 넣음.
+// digits.slice(1)은 매번 새로운 문자열을 생성하기 때문에 약간의 성능 손실이 있을 수 있습니다.
+// 인덱스 기반으로 처리하면 메모리 사용량을 줄일 수 있습니다.
+// 개선된 함수
+
+function addString2(str, digits, index, result) {
+  if (digits.length === index) {
+    result.push(str);
+    return;
+  }
+
+  const currentDict = dict[Number(digits[index])];
+  for (let i = 0; i < currentDict.length; i++) {
+    addString2(str + currentDict[i], digits, index + 1, result);
+  }
+}
 
 console.log(letterCombinations("23")); // output : ["ad","ae","af","bd","be","bf","cd","ce","cf"]
 console.log(letterCombinations("2")); // output : ["a","b","c"]
