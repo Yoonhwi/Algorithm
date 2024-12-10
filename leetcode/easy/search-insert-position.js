7f6c45badc4b574f99c67ac4d4abff5c975f20d2
Yoonhwi <ush0105@naver.com>
2024-12-10 15:53:26 +0900

    leetcode : [easy] 35번 in JS

---
+++ leetcode/easy/search-insert-position.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function (nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    if (nums[mid] < target) {
+      left = mid + 1;
+    } else if (nums[mid] === target) {
+      return mid;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return left;
+};
+
+console.log(searchInsert([1, 3, 5, 6], 5));
+console.log(searchInsert([1, 3, 5, 6], 2));
+console.log(searchInsert([1, 3, 5, 6], 7));
