2ed6fde675b8de75363313de6f0c93fcb461f638
Yoonhwi <ush0105@naver.com>
2024-12-31 02:33:25 +0900

    leetcode : [easy] 27번  in JS

---
+++ leetcode/easy/27.remove-element.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function (nums, val) {
+  let idx = 0;
+
+  while (idx < nums.length) {
+    if (nums[idx] === val) {
+      nums.splice(idx, 1);
+    } else {
+      idx++;
+    }
+  }
+
+  return nums.length;
+};
